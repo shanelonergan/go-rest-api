f290ac6b8a0cbe940287f5878b408f310d0744bc
shanelonergan <sptlonergan@gmail.com>
2020-02-02 15:25:53 -0500

    routes

--- main.go
+++ main.go
@@ -101,14 +101,20 @@
 // 	log.Fatal(http.ListenAndServe(":8080", router))
 // }
 
-
 package main
 
-import {
-	"encoding/json"
-	"log"
-	"net/http"
-	"math/rand"
-	"strconv"
+import (
 	"github.com/gorilla/mux"
+)
+
+func main() {
+	// Init router
+	router := mux.NewRouter()
+
+	// route handlers / endpoints
+	router.HandleFunc("/api/books", getBooks).Methods("GET")
+	router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/api/books", createBook).Methods("POST")
+	router.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 }
