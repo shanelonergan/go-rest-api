bc3b8f0b8ff1354f2ea7c3b467099b9f7a1d49d0
shanelonergan <sptlonergan@gmail.com>
2020-02-01 22:43:26 -0500

    added struct and events

--- main.go
+++ main.go
@@ -17,3 +17,19 @@ func main() {
 	router.HandleFunc("/", homeLink)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
+
+type event struct {
+	ID          string `json:"ID"`
+	Title       string `json:"Title`
+	Description string `json:"Description"`
+}
+
+type allEvents []event
+
+var events = allEvents{
+	{
+		ID:          "1",
+		Title:       "Introduction to Golang",
+		Description: "Come join us for a chance to learn how golang works and get to eventually try it out",
+	},
+}
