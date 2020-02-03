557adc8e46b2d058e16850e4e1cf5148fb3777df
shanelonergan <sptlonergan@gmail.com>
2020-02-03 16:40:08 -0500

    capitalization errors

--- main.go
+++ main.go
@@ -130,7 +130,7 @@ var books []Book
 
 // get all books
 func getBooks(w http.ResponseWriter, r *http.Request) {
-	w.header().set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(books)
 }
 
